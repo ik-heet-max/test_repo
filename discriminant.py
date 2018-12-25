7c9d5e729f97c7ad3c4d0c719738876fa3b47e6b
ik-heet-max <45899688+ik-heet-max@users.noreply.github.com>
2018-12-25 23:26:42 +0300

    Update discriminant.py

--- discriminant.py
+++ discriminant.py
@@ -3,7 +3,7 @@ a = int(input("Enter a "))
 b = int(input("Enter b "))
 c = int(input("Enter c "))
 
-d = b**2 - 4 * a * c
+d = b**2 - 4*a*c
 if d < 0:
     print("Your equation has no real roots")
 elif d >= 0:
